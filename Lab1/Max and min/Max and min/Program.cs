3d3ac1ae67c12d7f330943541d2b3b4bc36f89f2
Yerken <muratovyerken@gmail.com>
2018-01-29 12:36:44 +0600

    randw

---
+++ Lab1/Max and min/Max and min/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_and_min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("pato.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string a = sr.ReadLine();
+            string[] b = a.Split(' ');
+            int max = int.Parse(b[0]);
+            int min = int.Parse(b[0]);
+            sr.Close();
+            fs.Close();
+            for(int i = 1;i < b.Length; i++)
+            {
+                if(int.Parse(b[i]) > max)
+                {
+                    max = int.Parse(b[i]);
+                }
+                if(int.Parse(b[i]) < min)
+                {
+                    min = int.Parse(b[i]);
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            
+
+        }
+    }
+}
